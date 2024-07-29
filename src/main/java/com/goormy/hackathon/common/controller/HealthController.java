e5d40235d94e634aa58876c7d9d2343eddb29b59
siyeonSon <siyeon0209@naver.com>
2024-07-30 00:02:48 +0900

    feat: health check API

---
+++ src/main/java/com/goormy/hackathon/common/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.goormy.hackathon.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping({"/health", "/"})
+    public String health() {
+        return "Application Health Good!";
+    }
+
+}
