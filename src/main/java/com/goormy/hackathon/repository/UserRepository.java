34d5a31d6e3bbcd46db872c97eaf6beb97756c57
seonghooni <lsh3717@naver.com>
2024-07-30 20:46:02 +0900

    feat: Post, Use 레포지토리 간단한 조회 코드 작성

---
+++ src/main/java/com/goormy/hackathon/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.goormy.hackathon.repository;
+
+import com.goormy.hackathon.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
